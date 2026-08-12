8c4274d1a3525b45a96e92f746f46c7f3ce48f15
ZyadEslam <zyadelbehiry@gmail.com>
2026-08-12 19:52:15 +0300

    fix: sections organization v1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Hero from "./Components/Sections/HeroSection";
 import Intro from "./Components/Sections/Intro";
 import About from "./Components/Sections/AboutSection";
-import TitlesMarquee from "./Components/Sections/TitlesMarquee";
+// import TitlesMarquee from "./Components/Sections/TitlesMarquee";
 // import Heroo from "./Components/Sections/PhoneShowcase";
 import ProjectsSection from "./Components/Sections/ProjectsSection";
 import ServicesSection from "./Components/Sections/ServicesSection";
@@ -13,14 +13,14 @@ import ProcessSection from "./Components/Sections/ProcessSection";
 import VelocityMarquee from "./Components/Sections/VelocityMarqueeSection";
 
 export default function Home() {
-  // const [introDone, setIntroDone] = useState(false);
+  const [introDone, setIntroDone] = useState(false);
 
-  // useEffect(() => {
-  //   document.body.style.overflow = introDone ? "" : "hidden";
-  // }, [introDone]);
+  useEffect(() => {
+    document.body.style.overflow = introDone ? "" : "hidden";
+  }, [introDone]);
   return (
     <>
-      {/* {!introDone && <Intro onComplete={() => setIntroDone(true)} />} */}
+      {!introDone && <Intro onComplete={() => setIntroDone(true)} />}
       <div className="relative">
         <div className="sticky top-0 -z-10 h-screen overflow-hidden">
           <Hero />
